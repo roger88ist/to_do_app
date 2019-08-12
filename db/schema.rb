2109274684cd36bb4330c70efacc21bd57cbdcac
Rogerio Istiraneopulos <roger88ist@gmail.com>
2019-08-12 07:43:15 -0400

    created model Group with attribute name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190812110035) do
+ActiveRecord::Schema.define(version: 20190812114212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "to_dos", force: :cascade do |t|
     t.string "task"
