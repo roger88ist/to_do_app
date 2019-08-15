448b19057364d0cd379e7a5d058a0d661fbe0ce4
Rogerio Istiraneopulos <roger88ist@gmail.com>
2019-08-15 12:55:54 -0400

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,75 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+list_of_todos = [
+  {
+    id: 1,
+    group: "Purchases",
+    task: "Go to the bank",
+    dependencyIds: [],
+    completedAt: nil
+  },
+  {
+    id: 2,
+    group: "Purchases",
+    task: "Buy hammer",
+    dependencyIds: [1],
+    completedAt: nil
+  },
+  {
+    id: 3,
+    group: "Purchases",
+    task: "Buy wood",
+    dependencyIds: [1],
+    completedAt: nil
+  },
+  {
+    id: 4,
+    group: "Purchases",
+    task: "Buy nails",
+    dependencyIds: [1],
+    completedAt: nil
+  },
+  {
+    id: 5,
+    group: "Purchases",
+    task: "Buy paint",
+    dependencyIds: [1],
+    completedAt: nil
+  },
+  {
+    id: 6,
+    group: "Build Airplane",
+    task: "Hammer nails into wood",
+    dependencyIds: [2, 3, 4],
+    completedAt: nil
+  },
+  {
+    id: 7,
+    group: "Build Airplane",
+    task: "Paint wings",
+    dependencyIds: [5, 6],
+    completedAt: nil
+  },
+  {
+    id: 8,
+    group: "Build Airplane",
+    task: "Have a snack",
+    dependencyIds: [],
+    completedAt: nil
+  }
+]
+
+['Purchases', 'Build Airplane'].each do |name|
+  Group.create(name: name)
+end
+
+list_of_todos.each do |todo|
+  group_id = Group.find_by(name: todo[:group]).id
+  created_todo = ToDo.create(name: todo[:task],
+                             group_id: group_id)
+  todo[:dependencyIds].each do |id|
+    Pretask.create(to_do_id: created_todo.id,
+                   task_id: id)
+  end
+end
