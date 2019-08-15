01e0fa74824b6c8e6dab866fb320e3f29076a59a
Rogerio Istiraneopulos <roger88ist@gmail.com>
2019-08-15 00:07:36 -0400

    added logic to views to either display an X or the checkbox based on whether or not the ToDo can be updated

--- app/controllers/to_do_controller.rb
+++ app/controllers/to_do_controller.rb
@@ -5,11 +5,28 @@ class ToDoController < ApplicationController
 
   def update
     to_do = ToDo.find(to_do_params[:id])
+    incomplete_ids = to_do.tasks.incomplete.pluck(:id)
+
+    response_hash = {}
+
+    if completing_to_do && incomplete_ids.present?
+      response_hash[:errors] = incomplete_ids
+    elsif completing_to_do
+      to_do.completed = true
+      to_do.save
+      response_hash[:success] = to_do.tasks.pluck(:id)
+    end
+
+    render json: response_hash.to_json
   end
 
   protected
 
   def to_do_params
     params.permit(:id, :completed)
+  end
+
+  def completing_to_do
+    to_do_params[:completed] == "true"    
   end
 end
--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,6 +1,10 @@
 class ToDo < ApplicationRecord
+
   belongs_to :group 
 
   has_many :pretasks
   has_many :tasks, through: :pretasks, class_name: "ToDo"
+
+  scope :incomplete, -> { where(completed: false) }
+
 end
